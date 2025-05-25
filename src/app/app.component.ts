8bfb38ae15ec2f40ca30f9205207d54aceff3e51
aldoht <aldofernandoht@gmail.com>
2025-05-25 01:12:23 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FileUploaderComponent} from './file-uploader/file-uploader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FileUploaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
